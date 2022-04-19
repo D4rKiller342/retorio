e8615e4a877aeca6189a05b154cead90a136b938
Rogue Tale Teller <williamraichecraft@gmail.com>
2022-04-19 08:19:41 -0400

    moddified some fille

--- configs/ore-configs.lua
+++ configs/ore-configs.lua
@@ -17,9 +17,9 @@ rog.ore = {
         --count = 1, --default is 1 this line can be remouved
         order = 'c'
     ]]--
-    ["test-ore"] = {
-        name = 'test-ore',
-        result = 'iron-gear-wheel',
+    ["nano-ore"] = {
+        name = 'nano-ore',
+        result = 'iron-gear-wheel', --add nano ore here
         icon = '__rogue-custom-ores__/graphics/icons/ic-ore.png',
         tint = nil,
         map_ore_tint = {r = 0, g = 0, b = 1},
@@ -27,8 +27,8 @@ rog.ore = {
         ore_graphic_hr = '__rogue-custom-ores__/graphics/entity/ores/ic-ore-hr.png',
         has_starting_area_placement = true,
         base_density = 10,
-        starting_rq_factor_multiplier = 1.5,
-        regular_rq_factor_multiplier = 1.10, --can be remouve default value is 1
+        starting_rq_factor_multiplier = 1.5, -- like iron
+        regular_rq_factor_multiplier = 1.10, -- like iron
         mining_time = 1, --default is 2.5 if not present this line can be remouved
         order = 'b'
     }
