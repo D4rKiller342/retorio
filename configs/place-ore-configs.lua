eab34c2bbe5a46752fdaa9ce65bd713cfebb7d6c
Rogue Tale Teller <williamraichecraft@gmail.com>
2022-04-21 08:43:32 -0400

    remouved useless comment

--- configs/place-ore-configs.lua
+++ configs/place-ore-configs.lua
@@ -1,22 +1,4 @@
 rog.ore = {
-    --[[
-        Model:
-        ["Ore-name"] = {
-        name = 'name',
-        result = 'result',
-        icon = 'icon file',
-        tint = nil, --only add if there's a tint to the sprite to add
-        map_ore_tint = {r = 0, g = 0, b = 1},
-        ore_graphic = 'non hr grapic',
-        ore_graphic_hr = 'hr grapic',
-        has_starting_area_placement = true,
-        --regular_rq_factor_multiplier = 1, --can be remouve default value is 1
-        --fluid_type = 'water', --only add if there's a liquid req
-        --fluid_amount = 50,  --only add if there's a liquid req
-        mining_time = 1, --default is 2.5 if not present this line can be remouved
-        --count = 1, --default is 1 this line can be remouved
-        order = 'c'
-    ]]
     ["nano-ore"] = {
         name = 'nano-ore',
         result = 'nano-ore',
--- functions/recipe-maker.lua
+++ functions/recipe-maker.lua
@@ -1,15 +1,5 @@
 function rog.recipereturn(recipe)
-    --[[local recipevar = {
-        {
-            --Maybe add normal and expensive
-            type = "recipe",
-            name = recipe.name,
-            enable = recipe.isEnabled or false,
-            energy_required = recipe.energy_requirement or 1,
-            ingredients = recipe.ingredients,
-            result = recipe.result,
-        }
-    }]]
+    --add more complex recipie posibility
     return 
     {
         type = "recipe",
